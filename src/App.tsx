04efc31f96e516c00774664501bc1b76b936c1c0
Alina <alina.kotyaeva@mail.ru>
2023-06-09 10:37:08 +0300

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ import Main from './pages/Main';
 import { ChakraProvider } from '@chakra-ui/react'
 import Authorization from './pages/Authorization';
 import ContactsPage from './pages/ContactsPage';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom';
 import { store } from './store';
 import { Provider } from 'react-redux';
 
 function App() {
   return (
     <ChakraProvider>
-      <BrowserRouter>
+      <HashRouter>
       <Provider store={store}>
         <Routes>
           <Route path='*' element = {<Authorization />} />
@@ -20,7 +20,7 @@ function App() {
       </Provider>
       
         
-      </BrowserRouter>
+      </HashRouter>
       
     </ChakraProvider>
   );
